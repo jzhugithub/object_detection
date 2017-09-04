2308df20386816e8a0b6e7813fe6c507ea375142
jzhugithub <mfx106825372@163.com>
2017-09-04 11:50:50 +0800

    using skimge to read image which is more fast, definite input and output Tensors for detection_graph before loop

--- object_detection_tutorial.py
+++ object_detection_tutorial.py
@@ -4,13 +4,8 @@ import six.moves.urllib as urllib
 import sys
 import tarfile
 import tensorflow as tf
-import zipfile
-
-from collections import defaultdict
-from io import StringIO
 from matplotlib import pyplot as plt
-from PIL import Image
-
+import skimage.io
 
 # Env setup
 # # This is needed to display the images.
@@ -73,12 +68,6 @@ categories = label_map_util.convert_label_map_to_categories(label_map, max_num_c
 category_index = label_map_util.create_category_index(categories)
 
 
-# Helper code
-def load_image_into_numpy_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
-
-
 # Detection
 # For the sake of simplicity we will use only 2 images:
 # image1.jpg
@@ -92,24 +81,24 @@ IMAGE_SIZE = (12, 8)
 
 with detection_graph.as_default():
     with tf.Session(graph=detection_graph) as sess:
+        # Definite input and output Tensors for detection_graph
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
         for image_path in TEST_IMAGE_PATHS:
-            image = Image.open(image_path)
             # the array based representation of the image will be used later in order to prepare the
             # result image with boxes and labels on it.
-            image_np = load_image_into_numpy_array(image)
+            image_np = skimage.io.imread(image_path)
             # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
             image_np_expanded = np.expand_dims(image_np, axis=0)
-            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-            # Each box represents a part of the image where a particular object was detected.
-            boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-            # Each score represent how level of confidence for each of the objects.
-            # Score is shown on the result image, together with the class label.
-            scores = detection_graph.get_tensor_by_name('detection_scores:0')
-            classes = detection_graph.get_tensor_by_name('detection_classes:0')
-            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
             # Actual detection.
-            (boxes, scores, classes, num_detections) = sess.run(
-                [boxes, scores, classes, num_detections],
+            (boxes, scores, classes, num) = sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
                 feed_dict={image_tensor: image_np_expanded})
             # Visualization of the results of a detection.
             vis_util.visualize_boxes_and_labels_on_image_array(
