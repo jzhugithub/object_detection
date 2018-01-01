c370102454552bc34d2bca8a2e9e91710a68d2ea
jzhugithub <mfx106825372@163.com>
2018-01-02 00:45:54 +0800

    add gradient_conv2d411()

--- object_detection/core/layers.py
+++ object_detection/core/layers.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
+
 slim = tf.contrib.slim
+import numpy as np
+
 
 def area_conv2d11(inputs, inputs_hwc, num_outputs, scope=None):
     '''
@@ -9,6 +12,7 @@ def area_conv2d11(inputs, inputs_hwc, num_outputs, scope=None):
     :param inputs: A 4-D Tensor with shape [batch, height, width, in_channels]
     :param inputs_hwc: A list of length 3: [height, width, in_channels]
     :param num_outputs: Integer, the number of output filters.
+    :param scope: String, scope name.
     :return: A 4-D Tensor with shape [batch, height, width, num_outputs]
     '''
 
@@ -34,3 +38,74 @@ def area_conv2d11(inputs, inputs_hwc, num_outputs, scope=None):
                              shape=[-1, inputs_height, inputs_width, num_outputs],
                              name='reshape2')
         return outputs
+
+
+def create_mask4(inputs_hwc):
+    '''
+    create ratio mask for four kernels.
+    
+    :param inputs_hwc: A list of length 3: [height, width, in_channels]
+    :return: A dict of four ratio mask.
+    '''
+
+    if inputs_hwc[0] != inputs_hwc[1]:
+        raise ValueError('create_mask4() only support same height and width now')
+
+    # mask00_init = np.zeros(inputs_hwc, np.float32)
+    mask00_init = 1e-6 * np.ones(inputs_hwc, np.float32)
+    for row in range(inputs_hwc[0] - 1):
+        for col in range(inputs_hwc[1] - row - 1):
+            mask00_init[row, col, :] = 1.0 * (inputs_hwc[0] - row - col - 1) / (inputs_hwc[0] - 1)
+    mask01_init = mask00_init[::-1, :, :]
+    mask10_init = mask00_init[:, ::-1, :]
+    mask11_init = mask00_init[::-1, ::-1, :]
+
+    mask00_init_sum = mask00_init + mask01_init + mask10_init + mask11_init
+
+    mask00 = mask00_init / mask00_init_sum
+    mask01 = mask01_init / mask00_init_sum
+    mask10 = mask10_init / mask00_init_sum
+    mask11 = mask11_init / mask00_init_sum
+
+    return {'mask00': mask00,
+            'mask01': mask01,
+            'mask10': mask10,
+            'mask11': mask11}
+
+
+def gradient_conv2d411(inputs, inputs_hwc, num_outputs, scope=None):
+    '''
+    Implement gradient conv2d with ksizes=[1, 1, 1, 1], strides=[1, 1, 1, 1]
+    
+    :param inputs: A 4-D Tensor with shape [batch, height, width, in_channels]
+    :param inputs_hwc: A list of length 3: [height, width, in_channels]
+    :param num_outputs: Integer, the number of output filters.
+    :param scope: String, scope name.
+    :return: A 4-D Tensor with shape [batch, height, width, num_outputs]
+    '''
+
+    with tf.variable_scope(scope):
+        with slim.arg_scope([slim.conv2d],
+                            kernel_size=[1, 1],
+                            padding="SAME",
+                            activation_fn=None,
+                            normalizer_fn=None,
+                            normalizer_params=None):
+            if inputs_hwc[0] == 1 or inputs_hwc[1] == 1:
+                return slim.conv2d(
+                    inputs=inputs,
+                    num_outputs=num_outputs,
+                    scope='conv2d')
+            else:
+                # create ratio masks
+                masks = {}
+                for key, masknp in create_mask4([inputs_hwc[0], inputs_hwc[1], num_outputs]).items():
+                    masks[key] = (tf.constant(masknp, dtype=tf.float32, name=key))
+
+                # create net
+                net = []
+                for key, mask in masks.items():
+                    net.append(tf.expand_dims(mask, 0) * slim.conv2d(inputs=inputs,
+                                                                     num_outputs=num_outputs,
+                                                                     scope='conv2d_%s' % key))
+                return sum(net)
--- object_detection/core/layers_test.py
+++ object_detection/core/layers_test.py
@@ -1,22 +1,55 @@
 import tensorflow as tf
 import numpy as np
-from layers import area_conv2d11
+from layers import *
 
-images_np = np.array([[[[1111, 1211], [1112, 1212], [1113, 1213]],
-                       [[1121, 1221], [1122, 1222], [1123, 1223]],
-                       [[1131, 1231], [1132, 1232], [1133, 1233]]],
+def area_conv2d11_test():
+    images_np = np.array([[[[1111, 1211], [1112, 1212], [1113, 1213]],
+                           [[1121, 1221], [1122, 1222], [1123, 1223]],
+                           [[1131, 1231], [1132, 1232], [1133, 1233]]],
 
-                      [[[2111, 2211], [2112, 2212], [2113, 2213]],
-                       [[2121, 2221], [2122, 2222], [2123, 2223]],
-                       [[2131, 2231], [2132, 2232], [2133, 2233]]]])
+                          [[[2111, 2211], [2112, 2212], [2113, 2213]],
+                           [[2121, 2221], [2122, 2222], [2123, 2223]],
+                           [[2131, 2231], [2132, 2232], [2133, 2233]]]])
 
-images = tf.placeholder(tf.float32, images_np.shape)
+    images = tf.placeholder(tf.float32, images_np.shape)
 
-out = area_conv2d11(images, images_np.shape[1:], 6, 'AreaConv2d11')
+    out = area_conv2d11(images, images_np.shape[1:], 6, 'AreaConv2d11')
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    result = sess.run([out],feed_dict={images: images_np})
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        result = sess.run([out],feed_dict={images: images_np})
 
-    print(result[0])
-    print(np.array(result[0]).shape)
+        print(result[0])
+        print(np.array(result[0]).shape)
+
+
+def create_mask4_test():
+    inputs_hwc = [2,2,1]
+    print(create_mask4(inputs_hwc))
+
+def gradient_conv2d411_test():
+    images_np = np.array([[[[1111, 1211], [1112, 1212], [1113, 1213]],
+                           [[1121, 1221], [1122, 1222], [1123, 1223]],
+                           [[1131, 1231], [1132, 1232], [1133, 1233]]],
+
+                          [[[2111, 2211], [2112, 2212], [2113, 2213]],
+                           [[2121, 2221], [2122, 2222], [2123, 2223]],
+                           [[2131, 2231], [2132, 2232], [2133, 2233]]]])
+
+    images = tf.placeholder(tf.float32, images_np.shape)
+
+    outputs = gradient_conv2d411(images, images_np.shape[1:], 6, 'GradientConv2d411')
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        result = sess.run([outputs],feed_dict={images: images_np})
+
+        print(result[0])
+        print(np.array(result[0]).shape)
+
+
+# area_conv2d11_test()
+# create_mask4_test()
+gradient_conv2d411_test()
+
+
